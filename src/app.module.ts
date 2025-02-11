d7e6d35c45462553b10ec800d08bd1e29b6ad5c1
Tobias de Castilho <dev@tobiasdecastilho.com.br>
2025-02-10 23:24:05 -0300

    chore: fix name

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 
 /* Project */
 import { JwtService } from '@nestjs/jwt';
-import { AuthModule } from './modules/auth/auth.module';
 import { PrismaService } from './services/prisma.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UserModule],
   providers: [PrismaService, JwtService]
 })
 export class AppModule {}
