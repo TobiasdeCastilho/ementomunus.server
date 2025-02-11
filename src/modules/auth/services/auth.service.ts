fabffeb52a893c19f2873d6e9c71e05f26b09dfe
Tobias de Castilho <dev@tobiasdecastilho.com.br>
2025-02-10 22:43:11 -0300

    fix: types and uses

--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -1,55 +1,61 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
-import * as Prisma from '@prisma/client'
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import * as Prisma from '@prisma/client';
+import bcrypt from 'bcrypt';
 
 /* Project */
-import { EXCEPTIONS } from 'types/utils/exceptions'
-import { APIException } from '../../../utils/exceptions.utils'
-import { PrismaService } from '../../../services/prisma.service'
-import { JwtService } from '@nestjs/jwt'
+import { EXCEPTIONS } from 'types/utils/exceptions';
+import { APIException } from '../../../utils/exceptions.utils';
+import { PrismaService } from '../../../services/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-	constructor(
-		private readonly prisma: PrismaService,
-		private readonly jwt: JwtService,
-	) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwt: JwtService
+  ) {}
 
-	private async getToken(user: Prisma.User): Promise<{ token: string }> {
-		const token = this.jwt.sign({ name: user.name, email: user.email })
-		return { token }
-	}
+  private async getToken(user: Prisma.User): Promise<{ token: string }> {
+    const token = this.jwt.sign({ name: user.username, email: user.email });
+    return { token };
+  }
 
-	async login(payload: {
-		email: string
-		password: string
-	}): Promise<{ token: string }> {
-		const user = await this.prisma.user.findUnique({
-			where: { email: payload.email },
-		})
+  async login(payload: {
+    email: string;
+    password: string;
+  }): Promise<{ token: string }> {
+    const user = await this.prisma.user.findUnique({
+      where: { email: payload.email }
+    });
 
-		if (!user || user.password !== payload.password)
-			throw new APIException(EXCEPTIONS.USER_OR_PASSWORD_INCORRECT)
+    if (!user || user.password !== payload.password)
+      throw new APIException(EXCEPTIONS.USER_OR_PASSWORD_INCORRECT);
 
-		return this.getToken(user)
-	}
+    return this.getToken(user);
+  }
 
-	async register(payload: { email: string; password: string }): Promise<void> {
-		const user = await this.prisma.user.findUnique({
-			where: { email: payload.email },
-		})
+  async register(payload: {
+    username: string;
+    email: string;
+    password: string;
+  }): Promise<void> {
+    const user = await this.prisma.user.findUnique({
+      where: { email: payload.email }
+    });
 
-		if (user) throw new APIException(EXCEPTIONS.USER_ALREADY_EXISTS)
+    if (user) throw new APIException(EXCEPTIONS.USER_ALREADY_EXISTS);
 
-		await this.prisma.user.create({
-			data: {
-				email: payload.email,
-				password: payload.password,
-			},
-		})
-	}
+    await this.prisma.user.create({
+      data: {
+        username: payload.username,
+        email: payload.email,
+        password: bcrypt.hashSync(payload.password, 10)
+      }
+    });
+  }
 
-	forgot(payload: { email: string; password: string }): string {
-		throw new HttpException('Not implemented', HttpStatus.NOT_IMPLEMENTED)
-	}
+  forgot(payload: { email: string; password: string }): string {
+    throw new HttpException('Not implemented', HttpStatus.NOT_IMPLEMENTED);
+  }
 }
 
--- types/api/request/auth.request.ts
+++ types/api/request/auth.request.ts
@@ -1,20 +1,21 @@
-import { z } from 'zod'
+import { z } from 'zod';
 
 export const Login = z.object({
-	email: z.string().email(),
-	password: z.string(),
-})
-export type Login = z.infer<typeof Login>
+  email: z.string().email(),
+  password: z.string()
+});
+export type Login = z.infer<typeof Login>;
 
 export const Register = z.object({
-	email: z.string().email(),
-	password: z.string(),
-})
-export type Register = z.infer<typeof Register>
+  username: z.string(),
+  email: z.string().email(),
+  password: z.string()
+});
+export type Register = z.infer<typeof Register>;
 
 export const Forgot = z.object({
-	email: z.string().email(),
-	password: z.string(),
-})
-export type Forgot = z.infer<typeof Forgot>
+  email: z.string().email(),
+  password: z.string()
+});
+export type Forgot = z.infer<typeof Forgot>;
 
