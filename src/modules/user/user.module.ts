3794495d23ded1f3d221bef4356393101e126535
Tobias de Castilho <dev@tobiasdecastilho.com.br>
2025-02-11 00:00:57 -0300

    feat: create user.module

---
+++ src/modules/user/user.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+
+/* Project */
+import { PrismaService } from 'src/services/prisma.service';
+import { UserController } from './controllers/user.controller';
+import { UserService } from './services/user.service';
+
+@Module({
+  controllers: [UserController],
+  providers: [UserService, PrismaService],
+  exports: [UserService]
+})
+export class UserModule {}
+
