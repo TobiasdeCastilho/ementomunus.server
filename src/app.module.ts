e91fe5475b9d2a1c60387b173d01fd300b2d3468
Tobias de Castilho <dev@tobiasdecastilho.com.br>
2025-02-11 00:17:52 -0300

    feat: add quest module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from './services/prisma.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
+import { QuestModule } from './modules/quest/quest.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, QuestModule],
   providers: [PrismaService, JwtService]
 })
 export class AppModule {}
